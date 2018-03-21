9c3d5b82f305a4cd30c0b00e0d42c985d9583b9b
Matthew Wohlbach <mwohlbach@Matthews-MacBook-Pro.local>
2018-03-21 16:15:05 -0400

    test for post

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import http.server
-import socketserver
+from flask import Flask, request
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
 
 
+@app.route('/idk', methods=['POST'])
+def idk():
+    if request.method == 'POST':
+        request
+        return "hi mom" + request.form['file'] + request.form['dest']
+
+
+
+if __name__ == '__main__':
+    app.run(port=8081)
